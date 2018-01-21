51c51c4a325781d3ed92456882cec4c7e94a1c93
Potatoe <30378179+PotatoeTrainYT@users.noreply.github.com>
2018-01-21 13:06:40 +0000

    lots of stuff

--- src/CLACore/Core.php
+++ src/CLACore/Core.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace CLACore;
 
 use pocketmine\Player;
@@ -40,96 +42,74 @@ class Core extends PluginBase{
     public $cfg;
     public $money;
 
-    public function onEnable(){
-        $this->RegConfig();
-        $this->RegEvents();
-        $this->RegCommands();
-        $this->RegEconomy();
-        $this->RegTasks();
-        $this->getLogger()->info(C::GREEN."Enabled.");
+    public function onEnable() : void {
+        $this->registerConfigs();
+        $this->registerEvents();
+        $this->registerCommands();
+        $this->registerEconomy();
+        $this->registerTasks();
+        $this->getLogger()->info(C::GREEN . "Enabled");
     }
 
-    public function onDisable(){
-        $this->getLogger()->info(C::RED."Disabled.");
+    public function onDisable() : void {
+        $this->getLogger()->info(C::RED . "Disabled");
     }
 
-    public function RegConfig(){
+    public function registerConfigs() {
         @mkdir($this->getDataFolder());
         $this->saveResource("config.yml");
         $this->saveResource("rank.yml");
         $this->saveResource("title.yml");
         $this->saveResource("money.yml");
         $this->money = new Config($this->getDataFolder() . "money.yml", Config::YAML);
-        $this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
    }
 
-    public function RegEvents(){
-        if($this->cfg->get("Allow-Rank") == true){
-            $this->getServer()->getPluginManager()->registerEvents(($this->Rank = new Rank($this)), $this);
+    public function registerEvents() {
+        if($this->config->get("Allow-Rank") == true) {
+            new Rank($this);
         }
-        $this->getServer()->getPluginManager()->registerEvents(($this->onRespawnEvent = new onRespawnEvent($this)), $this);
-        $this->getServer()->getPluginManager()->registerEvents(($this->onJoinEvent = new onJoinEvent($this)), $this);
-        $this->getServer()->getPluginManager()->registerEvents(($this->onLoginEvent = new onLoginEvent($this)), $this);
-        $this->getServer()->getPluginManager()->registerEvents(($this->onExhaustEvent = new onExhaustEvent($this)), $this);
-        $this->getServer()->getPluginManager()->registerEvents(($this->onMoveEvent = new onMoveEvent($this)), $this);
+        new onRespawnEvent($this);
+        new onJoinEvent($this);
+        new onLoginEvent($this);
+        new onExhaustEvent($this);
+        new onMoveEvent($this);
     }
 
-    private function RegCommands(){
-        $this->getServer()->getCommandMap()->register("Spawn", new Spawn("Spawn", $this));
-        $this->getServer()->getCommandMap()->register("ping", new Ping("ping", $this));
-        $this->getServer()->getCommandMap()->register("fly", new Fly("fly", $this));
+    private function registerCommands() {
+        new Spawn($this);
+        new Ping($this);
+        new Fly($this);
     }
 
-    private function RegEconomy(){
-        if($this->cfg->get("Allow-Money") == true){
-            $this->getServer()->getCommandMap()->register("addmoney", new AddMoney("addmoney", $this));
-            $this->getServer()->getCommandMap()->register("takemoney", new TakeMoney("takemoney", $this));
-            $this->getServer()->getCommandMap()->register("setmoney", new SetMoney("setmoney", $this));
-            $this->getServer()->getCommandMap()->register("seemoney", new SeeMoney("seemoney", $this));
-            $this->getServer()->getCommandMap()->register("money", new Money("money", $this));
+    private function registerEconomy() {
+        if($this->cfg->get("Allow-Money") == true) {
+            new AddMoney($this);
+            new TakeMoney($this);
+            new SetMoney($this);
+            new SeeMoney($this);
+            new Money($this);
         }
     }
 
-    private function RegTasks(){
+    private function registerTasks(){
         if ($this->cfg->get("Enable-HighPingKick") == true){
-            $this->getServer()->getScheduler()->scheduleRepeatingTask(new HighPingCheckTask($this), 100); //5 Seconds.
+            new HighPingCheckTask($this);
         }
     }
 
-    public function myMoney($player){
-        if ($player instanceof Player) {
-            $player = $player->getName();
-        }
-        $player = strtolower($player);
-        $moneyconf = new Config($this->getDataFolder() . "money.yml", Config::YAML);
-        $moneyconf->get($player);
-        return $moneyconf->get($player);
+    public function myMoney(Player $player){
+        return $this->money->get(strtolower($player->getName()));
     }
 
-    public function reduceMoney($player, $money){
-        if ($player instanceof Player) {
-            $player->getName();
-        }
-        if ($this->myMoney($player) - $money < 0) {
-            return true;
-        }
-        $player = strtolower($player);
-        $moneyconf = new Config($this->getDataFolder() . "money.yml", Config::YAML);
-        $moneyconf->set($player, (int)$moneyconf->get($player) - $money);
-        $moneyconf->save();
+    public function reduceMoney(Player $player, Integer $money) {
+        $this->money->set(strtolower($player->getName()), (int)$this->money->get(strtolower($player->getName())) - $money);
+        $this->money->save();
         return true;
     }
 
-    public function addMoney($player, $money){
-        if ($player instanceof Player) {
-            $player->getName();
-        }
-        if ($this->myMoney($player) + $money < 0) {
-            return true;
-        }
-        $player = strtolower($player);
-        $moneyconf = new Config($this->getDataFolder() . "money.yml", Config::YAML);
-        $moneyconf->set($player, (int)$moneyconf->get($player) + $money);
+    public function addMoney($player, $money) {
+        $this->money->set(strtolower($player->getName()), (int)$this->money->get(strtolower($player->getName())) + $money);
         $moneyconf->save();
         return true;
     }
